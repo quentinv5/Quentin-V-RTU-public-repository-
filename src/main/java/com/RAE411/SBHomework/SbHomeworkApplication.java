1277238e9d6b156e3fa9a3d50af81803d27d16c2
quentinv5 <90255024+quentinv5@users.noreply.github.com>
2021-11-24 14:28:35 +0200

    Add files via upload

---
+++ src/main/java/com/RAE411/SBHomework/SbHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.RAE411.SBHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RAE411/SBHomework/SbHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RAE411.SBHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
